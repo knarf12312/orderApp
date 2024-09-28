58f6045a88f7121480348d5addeec6ecb7d867d3
knarf12312 <109194957+knarf12312@users.noreply.github.com>
2024-09-28 11:35:40 -0700

    1
    1

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'order-app',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader collapse='fade'>
+        <IonToolbar>
+          <IonTitle>Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen >
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blanks</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
